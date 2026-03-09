7c8c1936b51aa84a0ec8ee32eaa1d41eda553389
gagan <gaganjio14@gmail.com>
2026-03-09 13:12:11 +0530

    Initial project setup

---
+++ src/main/java/com/tms/dao/UserDAO.java
@@ -0,0 +1,15 @@
+package com.tms.dao;
+
+import com.tms.model.User;
+import com.tms.util.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class UserDAO {
+    public void insertUser(User user){
+
+    }
+}
---
+++ src/main/java/com/tms/model/Ticket.java
@@ -0,0 +1,13 @@
+package com.tms.model;
+
+public class Ticket {
+    private int id;
+    private String name;
+    private String email;
+    private String role;
+
+    Ticket(){
+
+    }
+
+}

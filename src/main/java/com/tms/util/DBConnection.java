61eefac2354d4eebfbecca56b18805a71a63f1c8
gagan <gaganjio14@gmail.com>
2026-03-09 13:13:32 +0530

    DB connection setup

--- src/main/java/com/tms/util/DBConnection.java
+++ src/main/java/com/tms/util/DBConnection.java
@@ -1,2 +1,15 @@
 package com.tms.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/ticket-management-system";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Gaganjio@014";
+
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
+}

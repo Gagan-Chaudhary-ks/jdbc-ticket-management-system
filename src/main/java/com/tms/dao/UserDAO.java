c273fbbd937f7ef4c1682cd630146655e1db0b36
gagan <gaganjio14@gmail.com>
2026-03-16 15:48:35 +0530

    Implement createUser() method in UserDAO using JDBC

--- src/main/java/com/tms/dao/UserDAO.java
+++ src/main/java/com/tms/dao/UserDAO.java
@@ -5,11 +5,48 @@ import com.tms.util.DBConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import java.util.List;
+import java.util.ArrayList;
 
 public class UserDAO {
-    public void insertUser(User user){
+    public void createUser(User user){
+        Connection conn = null;
+        PreparedStatement ps = null;
 
+        String query = "INSERT INTO users (name, email, role) VALUES (?, ?, ?)" ;
+
+        try{
+
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(query);
+
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getRole());
+
+            ps.executeUpdate();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        finally{
+            try{
+                if(ps != null){
+                    ps.close();
+                }
+
+                if(conn != null){
+                    conn.close();
+                }
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
     }
+
 }

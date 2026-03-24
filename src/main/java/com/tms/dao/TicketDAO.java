075cb663799af74098c419f5f15917ea180dd20a
gagan <gaganjio14@gmail.com>
2026-03-24 13:14:11 +0530

    Implement create ticket method in TicketDAO

--- src/main/java/com/tms/dao/TicketDAO.java
+++ src/main/java/com/tms/dao/TicketDAO.java
@@ -8,5 +8,39 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class TicketDAO {
+    public void createTicket(Ticket ticket){
+        Connection conn = null;
+        PreparedStatement ps = null;
 
+        String query = "INSERT INTO tickets (title, description, created_by, assigned_to, status, priority) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try{
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(query);
+
+            ps.setString(1, ticket.getTitle());
+            ps.setString(2, ticket.getDescription());
+            ps.setInt(3, ticket.getCreatedBy());
+            ps.setInt(4,ticket.getAssignedTo());
+            ps.setString(5,ticket.getStatus());
+            ps.setString(6,ticket.getPriority());
+
+            ps.executeUpdate();
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            try{
+                if(ps != null){
+                    ps.close();
+                }
+                if(conn != null){
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }

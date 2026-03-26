898bfc5e72808b6f0b05c8efb27bb481df8fce4e
gagan <gaganjio14@gmail.com>
2026-03-26 14:28:52 +0530

    Initial Main class structure setup

---
+++ src/main/java/com/tms/main/Main.java
@@ -0,0 +1,11 @@
+package com.tms.main;
+
+import com.tms.dao.TicketDAO;
+import com.tms.dao.UserDAO;
+import com.tms.model.Ticket;
+import com.tms.model.User;
+
+import java.util.List;
+
+public class Main {
+}

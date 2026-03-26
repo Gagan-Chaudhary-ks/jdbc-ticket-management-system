bee9c3e403c2a0785881bf417f4b7026952fb9bc
gagan <gaganjio14@gmail.com>
2026-03-26 14:27:41 +0530

    DBConnection updated to prevent password leaks

--- src/main/java/com/tms/util/DBConnection.java
+++ src/main/java/com/tms/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/ticket-management-system";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Gaganjio@014";
+    private static final String URL = "jdbc:mysql://localhost:3306/name_of_your_project";
+    private static final String USER = "username";
+    private static final String PASSWORD = "your_password";
 
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(URL,USER,PASSWORD);

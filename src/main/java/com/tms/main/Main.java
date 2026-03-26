d8bdc636d31373a746cb823f4a2ee0849ed63043
gagan <gaganjio14@gmail.com>
2026-03-26 14:29:37 +0530

    Main class updated

--- src/main/java/com/tms/main/Main.java
+++ src/main/java/com/tms/main/Main.java
@@ -8,4 +8,32 @@ import com.tms.model.User;
 import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
+
+        TicketDAO ticketDAO = new TicketDAO();
+        UserDAO userDAO = new UserDAO();
+
+        // Create User
+        User user = new User(0, "Anmol", "anmol93@gmail.com", "CREATOR");
+        userDAO.createUser(user);
+
+        // Create Ticket
+        Ticket ticket = new Ticket();
+        ticket.setTitle("Server Down");
+        ticket.setDescription("API not working");
+        ticket.setCreatedBy(3);
+        ticket.setAssignedTo(1);
+        ticket.setStatus("OPEN");
+        ticket.setPriority("HIGH");
+
+        ticketDAO.createTicket(ticket);
+
+        // Fetch Tickets
+        List<Ticket> tickets = ticketDAO.getAllTickets();
+
+        for(Ticket t : tickets){
+            System.out.println(t);
+        }
+    }
+
 }
